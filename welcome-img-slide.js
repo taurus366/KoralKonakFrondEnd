e40f2d1544a867217727da017768d05a891a27c4
Arcerr <taurus..ali47>
2024-01-28 14:31:22 +0200

    first commit

---
+++ welcome-img-slide.js
@@ -0,0 +1,21 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+function nextSlide() {
+    console.log("test");
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 10000); // Change slide every 5 seconds
