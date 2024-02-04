fb0daebce8b9600c119e1a3a3819f15c31e5d3ee
Arcerr <taurus..ali47>
2024-02-04 18:11:34 +0200

    .

---
+++ modals.js
@@ -0,0 +1,53 @@
+// ABOUT
+$('.box-menu-one').on('click', function () { 
+    $('#about').modal('toggle');
+ });
+
+ $('.about-modal-close').on('click', function(){
+    $('#about').modal('toggle');
+ });
+ // ABOUT
+
+ // CONTACTS
+$('.box-menu-three').on('click', function () { 
+   $('#contacts').modal('toggle');
+});
+
+$('.contacts-modal-close').on('click', function(){
+   $('#contacts').modal('toggle');
+});
+// CONTACTS
+
+// GALLERY
+$('.box-menu-five').on('click', function () { 
+   $('#gallery').modal('toggle');
+});
+
+$('.gallery-modal-close').on('click', function(){
+   $('#gallery').modal('toggle');
+});
+// GALLERY
+// IMG OF GALLERY
+$('.gallery-item').on('click', function () { 
+
+   // SET INFO THEN OPEN
+
+
+   $('#img-gallery').modal('toggle');
+});
+
+$('.img-gallery-modal-close').on('click', function(){
+   // CLEAR INFO THEN CLOSE
+   $('#img-gallery').modal('toggle');
+});
+// IMG OF GALLERY
+// RESERVATION
+$('.btn-reservation').on('click', function () { 
+   $('#reservation').modal('toggle');
+});
+
+$('.reservation-modal-close').on('click', function(){
+   $('#reservation').modal('toggle');
+});
+// RESERVATION
+
